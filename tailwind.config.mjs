ee4485ee52c292a38224fc6fd01a82b1269ceead
Esteban Vertzner <esteban.vertzner@canda.com>
2024-11-14 16:04:05 +0100

    finished

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      screens: {
+        '2xl': '1440px'
+      }
+    }
   },
   plugins: []
 };
